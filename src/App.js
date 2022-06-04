b4b46d94bf1f9b647bde2a953ac1771edda5d838
samleung <samleung@samleungs-Virtual-Machine.local>
2022-06-04 05:44:38 -0700

    add functions to extract and manipulate  data from AirTable API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.scss';
+import useApplicationData from './hooks/useApplicationData';
 
 function App() {
+  const { state } = useApplicationData();
+  console.log('state', state)
   return (
     <div className="App">
     </div>
