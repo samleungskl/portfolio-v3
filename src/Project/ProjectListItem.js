2ecc9b7251ef7cfed175dfd5eac592db5e5f7f18
samleung <samleung@samleungs-Virtual-Machine.local>
2022-06-07 11:58:27 -0700

    add featured logic and css

--- src/Project/ProjectListItem.js
+++ src/Project/ProjectListItem.js
@@ -1,16 +1,20 @@
 import './ProjectListItem.scss';
 import ProjectStackList from './ProjectStackList/ProjectStackList';
 import ProjectButtonList from './ProjectButtonList/ProjectButtonList';
-function ProjectListItem({ projectName, projectStack, projectStatus, projectDescription, projectButtons }) {
+function ProjectListItem({ projectName, projectStack, projectStatus, projectDescription, projectButtons, projectIsFeatured }) {
+  console.log({ projectIsFeatured })
   return (
     <div className="ProjectListItem">
-      <div className='projectNameAndStatusContainer'>
-        <h2 className='projectName'>{projectName}</h2>
-        <h3 className='projectStatus'>{projectStatus}</h3>
+      {projectIsFeatured === true && <p className='projectIsFeatured'>Featured</p>}
+      <div className="ProjectListItemContainer">
+        <div className='projectNameAndStatusContainer'>
+          <h2 className='projectName'>{projectName}</h2>
+          <h3 className='projectStatus'>{projectStatus}</h3>
+        </div>
+        <ProjectStackList dataArray={projectStack} />
+        <div className='projectDescription'>{projectDescription}</div>
+        <ProjectButtonList dataArray={projectButtons} />
       </div>
-      <ProjectStackList dataArray={projectStack} />
-      <div className='projectDescription'>{projectDescription}</div>
-      <ProjectButtonList dataArray={projectButtons} />
     </div>
   );
 }
