2e743f7257ac3401bb9905174c85b9834d15685b
samleung <samleung@samleungs-Virtual-Machine.local>
2022-06-08 01:20:35 -0700

    relocate year and status

--- src/Project/ProjectList.js
+++ src/Project/ProjectList.js
@@ -3,7 +3,7 @@ import ProjectListItem from './ProjectListItem';
 
 function ProjectList({dataArray}) {
     const result = dataArray.map((element)=>{
-      return <ProjectListItem projectName={element.projectName} projectStack={element.projectStack} projectIsFeatured={element.projectIsFeatured} projectStatus={element.projectStatus} projectDescription={element.projectDescription} projectImage={element.projectImage} key={element.projectUid} projectButtons={element.projectButtons}/>
+      return <ProjectListItem projectName={element.projectName} projectStack={element.projectStack} projectIsFeatured={element.projectIsFeatured} projectStatus={element.projectStatus} projectDescription={element.projectDescription} projectImage={element.projectImage} key={element.projectUid} projectButtons={element.projectButtons} projectYear={element.projectYear}/>
     })
   return (
     <div className="ProjectList">
