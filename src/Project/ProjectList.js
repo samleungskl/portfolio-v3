56e868a41cdcec47fe26720afc5e3ec58de1f80e
samleung <samleung@samleungs-Virtual-Machine.local>
2022-06-27 13:25:50 -0700

    more semi colons

--- src/Project/ProjectList.js
+++ src/Project/ProjectList.js
@@ -5,12 +5,12 @@ import ProjectListItem from './ProjectListItem';
 function ProjectList({dataArray}) {
     const result = dataArray.map((element)=>{
       return <ProjectListItem projectName={element.projectName} projectStack={element.projectStack} projectIsFeatured={element.projectIsFeatured} projectStatus={element.projectStatus} projectDescription={element.projectDescription} projectImage={element.projectImage} key={element.projectUid} projectButtons={element.projectButtons} projectYear={element.projectYear}/>
-    })
+    });
   return (
     <div className="ProjectList">
         {result}
     </div>
   );
-}
+};
 
 export default ProjectList;
